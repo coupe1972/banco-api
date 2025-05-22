4db7baa8bb1d8d79cff221ca5612c5bed6016404
David Ruiz <david.ruiz@esolutions.com.ar>
2025-05-22 00:17:46 -0300

    se eliminan comentarios

--- src/main/java/com/local/bank/config/SwaggerConfig.java
+++ src/main/java/com/local/bank/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2) // o SWAGGER_2 si querés
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.local.bank.controller"))
                 .paths(PathSelectors.any())
